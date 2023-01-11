50e1aa56d3df95ea2b474002031e0089bdcee1d1
Kayck <kayckmatias04@gmail.com>
2023-01-11 19:10:17 -0300

    removed unused code

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,12 +1,3 @@
 $(window).on('load', function () {
     $('#pre-loader').fadeOut('slow');
 });
-
-function isaNumber(number){
-    number = (number) ? number : window.event;
-    var charCode = (number.which) ? number.which : number.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-        return false;
-    }
-    return true;
-}
